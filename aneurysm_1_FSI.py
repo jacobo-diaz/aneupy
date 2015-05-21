5362562388ee573c940a7b40a920415231fe2bbf
Jacobo Díaz García <jdiaz@udc.es>
2015-05-21 07:58:29 +0200

    Minor changes

--- aneurysm_1_FSI.py
+++ aneurysm_1_FSI.py
@@ -5,8 +5,8 @@ aneupy = reload(Abaqus)
 
 m = aneupy.Model('aneurysm', cfd=True)
 
-m.part_from_iges('aneurysm_outer.iges')
-m.part_from_iges('aneurysm_inner.iges')
+m.part_from_iges('aneurysm_solid.iges')
+m.part_from_iges('aneurysm_fluid.iges')
 
 m.create_assembly()
 
