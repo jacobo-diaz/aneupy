06f313cd1c3285a5debd0b125e3689ca0d97014f
Jacobo Díaz García <jdiaz@udc.es>
2015-05-22 09:00:41 +0200

    Added some methods to Abaqus

--- aneupy/Abaqus.py
+++ aneupy/Abaqus.py
@@ -1,5 +1,13 @@
-# PONER autoria
-
+# =============================================================================
+#
+# Abaqus.py
+#
+# Python module to generate fluid-structure interaction (FSI) models in Abaqus
+#
+# Jacobo Diaz - jdiaz@udc.es
+# 2015
+#
+# =============================================================================
 
 import os
 
@@ -24,33 +32,42 @@ from odbAccess import *
 from odbSection import *
 
 
-class Model():
+class Database(object):
 
-    def __init__(self, name, cfd=False, **kwargs):
-
-        self.parts = {}
-        self.instances = {}
-        self.name = name
-        self.cfd = cfd
-
+    def __init__(self, **kwargs):
         Mdb()
 
         for odb in session.odbs.values():
             odb.close()
+
+    def save(self, file):
+
+        file_path = os.path.dirname(file)
+
+        file_extension = '.cae'
+        file_name = os.path.basename(file.rsplit(file_extension, 1)[0])
+
+        mdb.saveAs(pathName=os.path.join(file_path, file_name + file_extension))
+
+
+class Model(object):
+
+    def __init__(self, name, cfd=False, **kwargs):
+
+        self.name = name
+        self.cfd = cfd
+
+        self.parts = {}
+        self.materials = {}
+        self.instances = {}
 
         if self.cfd:
             self.model = mdb.Model(name=name, modelType=CFD)
         else:
             self.model = mdb.Model(name=name, modelType=STANDARD_EXPLICIT)
 
-        if 'Model-1' in mdb.models:
+        if 'Model-1' in mdb.models and name != 'Model-1':
             del mdb.models['Model-1']
-
-    def save_model(self, path=None):
-        if not path:
-            path = self.name
-
-        mdb.saveAs(pathName=path + '.cae')
 
     def part_from_iges(self, iges_file, name=None):
 
@@ -62,6 +79,24 @@ class Model():
         self.parts[name] = self.model.PartFromGeometryFile(name=name,
             geometryFile=iges, combine=False, stitchTolerance=1.0,
             dimensionality=THREE_D, type=DEFORMABLE_BODY, convertToAnalytical=1, stitchEdges=1)
+
+    def add_material(self, name, **kwargs):
+        self.materials[name] = self.model.Material(name=name)
+        self.density = kwargs.get('density', self.density)
+
+        self.materials[name].Density(table=((1000.0, ), ))
+        self.materials[name].Viscosity(table=((0.001,), ))
+
+        modelo.materials[i].Elastic(table=((materiales[i]['E'], materiales[i]['Nu']), ))
+
+        mdb.models['aneurysm_fluid'].materials['Material-2'].Hyperelastic(
+            materialType=ISOTROPIC, testData=OFF, type=ARRUDA_BOYCE,
+            volumetricResponse=VOLUMETRIC_DATA, table=((1.0, 2.0, 3.0), ))
+
+        mdb.models['aneurysm_fluid'].HomogeneousFluidSection(name='Section-1', material='Material-1')
+        mdb.models['aneurysm_solid'].HomogeneousSolidSection(name='Section-1', material='Material-1', thickness=None)
+
+
 
     def create_assembly(self):
         for name, part in self.parts.items():
--- test/aneurysm_1_FSI.py
+++ test/aneurysm_1_FSI.py
@@ -10,11 +10,48 @@ aneupy = reload(Abaqus)
 # import aneupy
 # -----------------------------------------------------------------------------
 
-m = aneupy.Model('aneurysm', cfd=True)
+db = aneupy.Database()
 
-m.part_from_iges('aneurysm_solid.iges')
-m.part_from_iges('aneurysm_fluid.iges')
+f = aneupy.Model('aneurysm_fluid', cfd=True)
+s = aneupy.Model('aneurysm_solid')
 
-m.create_assembly()
+f.part_from_iges('aneurysm_fluid.iges')
+f.part_from_iges('aneurysm_solid.iges')
+s.part_from_iges('aneurysm_solid.iges')
 
-m.save_model()
+f.create_set(name='outer_surface', coord=, entity_type=face|edge)
+f.create_surface(name='outer_surface', coord=, entity_type=face|edge)
+f.create_set(name='front_face')
+f.create_set(name='end_face')
+
+f.add_material('water', density=1000., viscosity=1.E-3)
+f.add_material('blood', E=10, nu=.3)
+s.add_material('flesh', E=20, nu=.3)
+
+f.add_section(name='fluid', material='water')
+
+f.assign_section(section='fluid', set|surface)
+
+f.create_assembly()
+s.create_assembly()
+
+f.add_global_seed
+f.add_local_seed(set|surface, elements, space, ...)
+
+f.create_mesh()
+
+f.add_step(name='fluid')
+
+f.create_load
+f.create_BC
+
+# PRobar si cambiando las coordenadas del iges se cambia la posición en el assembly
+
+# 3. Optionally, ensure matching nodes at the interface regions.
+# 4. In each model, create a co-simulation interaction to specify the interface region and coupling schemes.
+# 5. Create a co-execution to identify the two models involved and specify the job parameters for each analysis.
+# 6. Submit the co-execution to perform the co-simulation.
+# 7. View the results of the co-simulation using overlay plots.
+
+
+db.save(file='aneurysm.cae')
